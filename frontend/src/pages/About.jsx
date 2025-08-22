6dc9a10301e89a7ff68a09b16d8bc369172dec18
Udanth <udanth.work@gmail.com>
2025-08-22 09:21:38 +0530

    contact and about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-      About
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>About <span className='text-gray-700 font-medium'>Us</span></p>
+      </div>
+      <div className='my-10 flex flex-col sm:flex-row gap-12'>
+        <img className='w-full sm:max-w-[300px] md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>
+            We are a healthcare platform committed to making quality medical services accessible and convenient for everyone. Our mission is to connect patients with experienced doctors across various specialities, ensuring timely consultations and trusted care.
+          </p>
+          <p>
+            With a focus on innovation and patient satisfaction, we bring technology and healthcare together. From online appointment booking to easy access to specialists, we strive to make the healthcare journey smoother, faster, and more reliable.
+          </p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>
+            To revolutionize healthcare delivery by creating a patient-first ecosystem that is accessible, affordable, and driven by trust. We envision a world where every individual receives the right medical care at the right time with ease and transparency.
+          </p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>Why <span className='text-gray-700 font-semibold'>Choose Us</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16  flex-col gap-5 text-[15px] hover:bg-gray-500 hover:text-white transition-all duration-500 text-gray-600 cursor-pointer'>
+          <b>Efficiency</b>
+          <p>Stramlined and appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex-col gap-5 text-[15px] hover:bg-gray-500 hover:text-white transition-all duration-500 text-gray-600 cursor-pointer'>
+          <b>Convinience</b>
+          <p>Access to a network of trusted healthcare professionals in your area.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex-col gap-5 text-[15px] hover:bg-gray-500 hover:text-white transition-all duration-500 text-gray-600 cursor-pointer'>
+          <b>Personalization</b>
+          <p>Tailored recommendations and reminders to help you on top of your health.</p>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
     <div>
-      Contact
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>Contact <span className='text-gray-700 font-semibold'>Us</span></p>
+      </div>
+
+      <div className='flex flex-col my-10 justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>Our Office</p>
+          <p className='text-gray-500'>some address in hyderabad, india.</p>
+          <p className='text-gray-500'>+91 0123456789 <br /> support@clinix.in</p>
+          <p className='font-semibold text-lg text-gray-600'>Careers</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration500 cursor-pointer'>Explore Jobs</button>
+        </div>
+      </div>
+
     </div>
   )
 }
