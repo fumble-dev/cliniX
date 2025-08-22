03f232f5455343e7f416ec72d5be6639264630a4
Udanth <udanth.work@gmail.com>
2025-08-22 10:28:16 +0530

    login and profile pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state, setState] = useState('Sign Up');
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+
+  const onSuubmitHandler = async (e) => {
+    e.preventDefault()
+
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={onSuubmitHandler} className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-2xl'>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</p>
+        <p className=''>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book appointment. </p>
+        {
+          state === 'Sign Up' &&
+          <div className='w-full'>
+            <p>Full Name</p>
+            <input className=' border border-zinc-300 rounded-lg w-full p-2 mt-1' type="text" placeholder='Enter your name.' value={name} onChange={(e) => setName(e.target.value)} required />
+          </div>
+        }
+        <div className='w-full'>
+          <p>Email</p>
+          <input className=' border border-zinc-300 rounded-lg w-full p-2 mt-1' type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter your email.' required />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input className=' border border-zinc-300 rounded-lg w-full p-2 mt-1' type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter your password.' required />
+        </div>
+        <button className='bg-black text-white w-full py-2 rounded-md text-base cursor-pointer hover:scale-105 transition-all duration-400'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        {
+          state === 'Sign Up' ?
+            <p>Already have an account? <span onClick={() => setState('Login')} className='text-blue-500 underline cursor-pointer'>Login Here</span></p> :
+            <p>Create a new account? <span onClick={() => setState('Sign Up')} className='text-blue-500 underline cursor-pointer'>Click Here</span></p>
+        }
+      </div>
+
+    </form>
   )
 }
 
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name: 'Udanth Reddy',
+    email: 'udanth@gmail.com',
+    image: assets.profile_pic,
+    phone: '+91 7386313880',
+    address: {
+      line1: "Hyderbad",
+      line2: "India"
+    },
+    gender: 'Male',
+    dob: '2005-04-22'
+  })
+
+  const [isEdit, setIsEdit] = useState(false);
+
   return (
-    <div>
-      MyProfile
+    <div className='max-w-lg flex flex-col gap-2 text-sm'>
+      <img className='w-36 rounded' src={userData.image} alt="" />
+      {
+        isEdit ?
+          <input className='bg-gray-200 text-3xl font-medium max-w-60 mt-4' value={userData.name} onChange={(e) => setUserData(prev => ({ ...prev, name: e.target.value }))} type='text' placeholder='Enter your name' /> :
+          <p className='font-medium text-3xl mt-4 text-neutral-800'>{userData.name}</p>
+      }
+
+      <hr className='bg-zinc-400 border-none h-[1px]' />
+      <div>
+        <p className='text-neutral-500 underline mt-3'>Contact Information</p>
+        <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
+          <p className='font-medium'>Email Id:</p>
+          <p className='text-blue-500'>{userData.email}</p>
+          <p className='font-medium'>Phone:</p>
+          {
+            isEdit ?
+              <input className='bg-gray-200 max-w-52' value={userData.phone} onChange={(e) => setUserData(prev => ({ ...prev, phone: e.target.value }))} type='text' placeholder='Enter your Phone Number' /> :
+              <p className='text-blue-400'>{userData.phone}</p>
+          }
+          <p className='font-medium'>Address:</p>
+          {
+            isEdit ?
+              <p>
+                <input className='bg-gray-200' value={userData.address.line1} onChange={(e) => setUserData(prev => ({ ...prev, address: { line1: e.target.value } }))} type="text" />
+                <br />
+                <input className='bg-gray-200' value={userData.address.line2} onChange={(e) => setUserData(prev => ({ ...prev, address: { line2: e.target.value } }))} type="text" />
+              </p> :
+              <p className='text-gray-500'>
+                {userData.address.line1}
+                <br />
+                {userData.address.line2}
+              </p>
+          }
+        </div>
+      </div>
+
+      <div>
+        <p className='text-neutral-500 underline mt-3'>Basic Information:</p>
+        <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
+          <p className='font-medium'>Gender:</p>
+          {
+            isEdit ?
+              <select className='max-w-20 bg-gray-200' value={userData.gender} onChange={(e) => setUserData(prev => ({ ...prev, gender: e.target.value }))}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+              :
+              <p className='text-gray-400'>{userData.gender}</p>
+          }
+          <p className='font-medium'>Birthday:</p>
+          {
+            isEdit ?
+              <input className='bg-gray-200' onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} type='date' /> :
+              <p className='text-gray-400'>{userData.dob}</p>
+          }
+        </div>
+      </div>
+
+      <div className='mt-10  '>
+        {
+          isEdit ?
+            <button className='bg-black border text-white text-sm py-2 px-8 rounded-full cursor-pointer ' onClick={() => setIsEdit(false)}>Save Information</button> :
+            <button className='bg-black border text-white text-sm py-2 px-8 rounded-full cursor-pointer ' onClick={() => setIsEdit(true)}>Edit</button>
+        }
+      </div>
+
     </div>
   )
 }
