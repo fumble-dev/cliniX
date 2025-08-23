844a232ec4b6d6167c5a12231afde078e56af3f6
Udanth <udanth.work@gmail.com>
2025-08-23 11:58:19 +0530

    added db  models

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import connectToDb from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
 
 const app = express()
 const port = process.env.PORT || 4000
+connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
