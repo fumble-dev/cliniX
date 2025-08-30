a15c495282842e6aa4bb67663101fdd7e201f29f
Udanth <udanth.work@gmail.com>
2025-08-30 08:01:33 +0530

    navbar and sidebar ui

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,13 @@ import React, { useContext } from 'react';
 import Login from './pages/Login';
 import { ToastContainer } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import AddDoctor from './pages/Admin/AddDoctor';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import DoctorsList from './pages/Admin/DoctorsList';
 
 const App = () => {
   const { atoken } = useContext(AdminContext);
@@ -9,7 +16,20 @@ const App = () => {
   return (
     <div>
       {atoken ? (
-        <h1>Welcome Admin! 🎉 (Dashboard goes here)</h1>
+        <div className='bg-[#F8F9FD]'>
+          <ToastContainer />
+          <Navbar />
+          <div className='flex items-start'>
+            <Sidebar />
+            <Routes>
+              <Route path='/' element={<></>} />
+              <Route path='/admin-dashboard' element={<Dashboard />} />
+              <Route path='/all-appointments' element={<AllAppointments />} />
+              <Route path='/add-doctor' element={<AddDoctor />} />
+              <Route path='/doctor-list' element={<DoctorsList />} />
+            </Routes>
+          </div>
+        </div>
       ) : (
         <Login />
       )}
